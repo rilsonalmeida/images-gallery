d26d6f95229d53fcbab2d7820c926e10e6b5ddec
Rilson Almeida <rilson.almeida@gmail.com>
2022-04-23 10:57:10 -0300

    spinner in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
+import Spinner from './components/Spinner';
 import Welcome from './components/Welcome';
 import { Container, Row, Col } from 'react-bootstrap';
 
@@ -14,11 +15,13 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 function App() {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getSavedImages() {
       const res = await axios.get(`${API_URL}/images`);
       setImages(res.data || []);
+      setLoading(false);
     }
     getSavedImages();
   }, []);
@@ -67,24 +70,34 @@ function App() {
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      <Container className="mt-4">
-        {images.length ? (
-          <Row xs={1} md={2} lg={3}>
-            {images.map((image, i) => (
-              <Col className="pb-3" key={i}>
-                <ImageCard
-                  image={image}
-                  deleteImage={handleDeleteImage}
-                  saveImage={handleSaveImage}
-                />
-              </Col>
-            ))}
-          </Row>
-        ) : (
-          <Welcome />
-        )}
-      </Container>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Search
+            word={word}
+            setWord={setWord}
+            handleSubmit={handleSearchSubmit}
+          />
+          <Container className="mt-4">
+            {images.length ? (
+              <Row xs={1} md={2} lg={3}>
+                {images.map((image, i) => (
+                  <Col className="pb-3" key={i}>
+                    <ImageCard
+                      image={image}
+                      deleteImage={handleDeleteImage}
+                      saveImage={handleSaveImage}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            ) : (
+              <Welcome />
+            )}
+          </Container>{' '}
+        </>
+      )}
     </div>
   );
 }
